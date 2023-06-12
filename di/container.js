c809bf3eee2423353190978a9b4d99285ce0f063
nilcux <chenglin.xu@unistra.fr>
2023-06-12 11:35:31 +0200

    Modify API design (add sub-collection for tasks)

--- di/container.js
+++ di/container.js
@@ -1,12 +1,12 @@
 const awilix = require('awilix');
 const container = awilix.createContainer();
 
-const CreateTaskHandler = require('../handlers/taskHandlers/createTaskHandler');
+const CreateT2ITaskHandler = require('../handlers/taskHandlers/createT2ITaskHandler');
 const GetAllModelsHandler = require('../handlers/modelHandlers/getAllModelsHandler');
 const GetOneModelHandler = require('../handlers/modelHandlers/getOneModelHandler')
 
 container.register({
-  createTaskHandler: awilix.asClass(CreateTaskHandler).singleton(),
+  createT2ITaskHandler: awilix.asClass(CreateT2ITaskHandler).singleton(),
   getAllModelsHandler: awilix.asClass(GetAllModelsHandler).singleton(),
   getOneModelHandler: awilix.asClass(GetOneModelHandler).singleton()
 });
